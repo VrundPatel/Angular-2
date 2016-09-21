bd777bb007a654c1ad77cba2d18af1c2b8af2f16
Vrund Patel <vrundp07@yahoo.com>
2016-09-20 20:13:37 -0400

    Removed css and test files

--- recipe-book/src/app/app.component.ts
+++ recipe-book/src/app/app.component.ts
@@ -5,7 +5,6 @@ import { HeaderComponent } from './header.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
 }
