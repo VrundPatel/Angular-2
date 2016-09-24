ccbe5603bb1cfd882ebef67aa635a7f6eb078859
Vrund Patel <vrundp07@yahoo.com>
2016-09-23 22:41:17 -0400

    Created recipes array and the html for it to loop.

--- recipe-book/src/app/recipes/recipe-item.component.ts
+++ recipe-book/src/app/recipes/recipe-item.component.ts
@@ -1,16 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 import { Recipe } from './recipe';
 
 @Component({
-  selector: 'app-recipe-item',
+  selector: 'rb-recipe-item',
   templateUrl: './recipe-item.component.html',
   styleUrls: ['./recipe-item.component.css']
 })
 
 export class RecipeItemComponent implements OnInit {
-  recipe;
-  recipeId;
+  @Input() recipe: Recipe;
+  recipeId: number;
 
   constructor() { }
 
